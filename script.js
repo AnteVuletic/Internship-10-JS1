75e9a922eadfd7af832ab9fe3eaa1fa6ad984f03
AnteVuletic <antevuletic1995@gmail.com>
2019-03-05 23:59:35 +0100

    Should be done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let choice
+let choice;
 while(choice !== "Kraj"){
     choice = prompt(`Odaberi redni broj algoritma kojeg zelis izvrsit \n
     1 \u27A4 Reverse \n
@@ -19,10 +19,32 @@ while(choice !== "Kraj"){
         case "4":
             alert(ReverseEachWord(AlgorithmInput()));
         break;
+        case null:
+        break;
+        default:
+            alert("Please choose an valid input");
+        break;
     }
 }
+
+function ItemComparer(a,b){
+    return a.height-b.height;
+}
+
+function ItemAvergeWeight(prop){
+    let sum = 0;
+    for(let starWarsCharacter of prop){
+        sum += starWarsCharacter.mass;
+    }
+    return sum/prop.length;
+}
+  
 function AlgorithmInput(){
-    return prompt("Unesite tekst za algoritam");
+    let response = prompt("Unesite tekst za algoritam");
+    if(response === null){
+        return 0;
+    }
+    return response;
 }
 function Reverse(prop){
     let buffer = "";
@@ -88,3 +110,59 @@ function ReverseEachWord(prop){
     }
     return buffer;
 }
+(()=>{
+    let array = [
+    {
+        name: "Luke Skywalker",
+        height: 172,
+        mass: 77,
+        hair_color: "blond",
+        skin_color: "fair",
+        eye_color: "blue",
+        birth_year: "19BBY",
+        gender: "male"
+    },
+    {
+        name: "C-3PO",
+        height: 167,
+        mass: 75,
+        hair_color: "n/a",
+        skin_color: "gold",
+        eye_color: "yellow",
+        birth_year: "112BBY",
+        gender: "n/a"
+    },
+    {
+        name: "R2-D2",
+        height: 96,
+        mass: 32,
+        hair_color: "n/a",
+        skin_color: "white, blue",
+        eye_color: "red",
+        birth_year: "33BBY",
+        gender: "n/a"
+    },
+    {
+        name: "Darth Vader",
+        height: 202,
+        mass: 136,
+        hair_color: "none",
+        skin_color: "white",
+        eye_color: "yellow",
+        birth_year: "41.9BBY",
+        gender: "male"
+    },
+    {
+        name: "Leia Organa",
+        height: 150,
+        mass: 49,
+        hair_color: "brown",
+        skin_color: "light",
+        eye_color: "brown",
+        birth_year: "19BBY",
+        gender: "female"
+    }];
+    array.sort(ItemComparer);
+    console.log(...array);
+    console.log("Averege mass of each starwars character is: ",ItemAvergeWeight(array))
+})();;
